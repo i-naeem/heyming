4f8879c4c70b050c2151d73b68cbb0d94c40f7b9
i-naeem <inaeem@protonmail.com>
2022-04-22 21:19:56 +0500

    :art: update bits array styles

--- src/components/BitsArray.js
+++ src/components/BitsArray.js
@@ -1,20 +1,35 @@
-import { ButtonGroup, Button } from '@chakra-ui/react';
+import { SimpleGrid, Box, Text, VStack } from '@chakra-ui/react';
 import isPowerOf2 from '../helpers/isPowerOfTwo';
 
 const BitsArray = ({ bitsArray = [] }) => {
   return (
-    <ButtonGroup spacing={1} size="lg">
-      {bitsArray.map((bit, bitIndex) => (
-        <Button
-          colorScheme="teal"
-          variant={isPowerOf2(bitIndex + 1) ? 'solid' : 'outline'}
-          borderRadius="none"
-          key={bitIndex}
-        >
-          {bit}
-        </Button>
-      ))}
-    </ButtonGroup>
+    <SimpleGrid spacing={1} columns={[4, 6, 8, 12]} size="lg">
+      {bitsArray.map((bit, index) => {
+        const bitIndex = index + 1;
+        const isParityBit = isPowerOf2(bitIndex);
+        return (
+          <Box
+            p="2"
+            d="flex"
+            border="1px"
+            key={bitIndex}
+            borderColor="teal"
+            justifyContent="center"
+            bgColor={isParityBit ? 'teal' : 'transparent'}
+          >
+            <VStack spacing={0.1}>
+              <Text fontSize="xl" fontWeight="bold">
+                {bit}
+              </Text>
+              <Text>
+                {isParityBit ? 'P' : 'D'}
+                {bitIndex}
+              </Text>
+            </VStack>
+          </Box>
+        );
+      })}
+    </SimpleGrid>
   );
 };
 
