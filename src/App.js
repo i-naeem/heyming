a3d928d7c0547e82e08af8ea99753b5de8709730
i-naeem <inaeem@protonmail.com>
2022-04-23 19:29:24 +0500

    :art: update layouts and add some features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
   Box,
+  Text,
   theme,
   Button,
   Heading,
   SimpleGrid,
   ChakraProvider,
-  Text,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import Header from './components/Header';
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 import UserInput from './components/UserInput';
 import BitsArray from './components/BitsArray';
 import useHammingCode from './services/HammingCode/useHammingCode';
+import isPowerOf2 from './helpers/isPowerOfTwo';
 
 function App() {
   const {
@@ -43,8 +44,24 @@ function App() {
             <UserInput onSubmit={onSubmit} isLoading={isLoading} />
           </Box>
 
+          {bitsArray.length ? (
+            <SimpleGrid columns={[1, 2]} mb="3" px="3">
+              <Box mb="2">
+                <Heading size="md">Efficiency</Heading>
+                <Text>
+                  Since we have some parity bits, not all of the bits can be
+                  used to transfer data. Our current efficiency is:
+                </Text>
+                <Text>
+                  {dataBitSize} data bits / {redundantBitsSize} ={' '}
+                  <strong>{efficiency}%</strong>
+                </Text>
+              </Box>
+            </SimpleGrid>
+          ) : null}
+
           {dataBits.length ? (
-            <Box as="section" px="2" mb="3">
+            <Box as="section" px="2" mb="2">
               <Heading fontSize="md" mb="2">
                 Data Bits
               </Heading>
@@ -66,24 +83,31 @@ function App() {
               </Box>
             </Box>
           ) : null}
+
           {bitsArray.length ? (
-            <Box as="section" p="3">
-              <SimpleGrid columns={[1, 2]} mb="3">
-                <Box mb="2">
-                  <Heading size="md">Efficiency</Heading>
-                  <Text>
-                    Since we have some parity bits, not all of the bits can be
-                    used to transfer data. Our current efficiency is:
-                  </Text>
-                  <Text>
-                    {dataBitSize} data bits / {redundantBitsSize} ={' '}
-                    <strong>{efficiency}%</strong>
-                  </Text>
-                </Box>
-              </SimpleGrid>
-              <Heading size="lg" mb="3">
+            <Box as="section" px="3">
+              <Heading size="lg" mb="2">
                 Hamming Code ({parity})
               </Heading>
+
+              {bitsArray.length < 20 ? (
+                <Box d="flex" flexWrap="wrap" mb="2">
+                  {bitsArray.map((bit, bitIndex) => {
+                    return (
+                      <Button
+                        m="1px"
+                        size="xs"
+                        key={bitIndex}
+                        colorScheme="blue"
+                        borderRadius="none"
+                        variant={isPowerOf2(bitIndex + 1) ? 'solid' : 'outline'}
+                      >
+                        {bit}
+                      </Button>
+                    );
+                  })}
+                </Box>
+              ) : null}
 
               <BitsArray bitsArray={bitsArray} />
             </Box>
